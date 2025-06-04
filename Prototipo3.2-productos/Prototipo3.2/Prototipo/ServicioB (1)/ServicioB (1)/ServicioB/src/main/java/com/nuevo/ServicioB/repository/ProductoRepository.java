9096d0e90f9935ebe0d48dcf14691cfc61e8fde7
ferandforest <167841254+ferandforest@users.noreply.github.com>
2025-06-04 01:58:36 -0400

    Delete Prototipo3.2-productos directory

--- Prototipo3.2-productos/Prototipo3.2/Prototipo/ServicioB (1)/ServicioB (1)/ServicioB/src/main/java/com/nuevo/ServicioB/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nuevo.ServicioB.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nuevo.ServicioB.model.Producto;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-
-}
