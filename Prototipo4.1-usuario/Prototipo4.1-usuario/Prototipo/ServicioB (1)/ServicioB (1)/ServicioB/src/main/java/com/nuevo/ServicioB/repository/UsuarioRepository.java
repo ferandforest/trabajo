f8efe1ce80eec092d05d120fff12257c330a85f7
ferandforest <167841254+ferandforest@users.noreply.github.com>
2025-06-04 01:59:04 -0400

    Delete Prototipo4.1-usuario/Prototipo4.1-usuario/Prototipo/ServicioB (1)/ServicioB (1)/ServicioB directory

--- Prototipo4.1-usuario/Prototipo4.1-usuario/Prototipo/ServicioB (1)/ServicioB (1)/ServicioB/src/main/java/com/nuevo/ServicioB/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nuevo.ServicioB.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nuevo.ServicioB.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-}
