bab1f94d318003c10194590eae03b2cbd05295e5
ferandforest <167841254+ferandforest@users.noreply.github.com>
2025-06-04 01:58:47 -0400

    Delete prototipo2.1-ventas directory

--- prototipo2.1-ventas/prototipo2.1/prototipo2.1/ServicioB/src/main/java/com/nuevo/ServicioB/repository/VentaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nuevo.ServicioB.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nuevo.ServicioB.model.Venta;
-
-public interface VentaRepository extends JpaRepository<Venta, Long> {
-}
